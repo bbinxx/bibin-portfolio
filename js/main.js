3a9e1d80b59dff57b018c203656f0243fb132cef
Bibin <bibinraju541@gmail.com>
2024-05-30 18:26:24 +0530

    logic for loading and tyepwriter

---
+++ js/main.js
@@ -0,0 +1,19 @@
+
+const textElement = document.getElementsByClassName('home')[0]; // Get the first element with class "typewriter"
+const text = "Hello! I am a dedicated Computer Science and Engineering student with a strong passion for technology and software development.";
+let i = 0;
+
+function typewriter() {
+  if (i < text.length) {
+    textElement.textContent += text.charAt(i); // Add next character using textContent (better for performance)
+    i++;
+    setTimeout(typewriter, 50); // Adjust speed here (in milliseconds)
+  }
+}
+window.onload = loading(),typewriter();
+function loading() {
+    const loadingScreen = document.getElementById('loading-screen');
+    loadingScreen.style.display = 'none'; // Hide the loading screen on page load
+  };
+  
+
